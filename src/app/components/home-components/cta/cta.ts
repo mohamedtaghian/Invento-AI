37b7d21238121e544982edb4f13cdf712382969f
AndrewAyman <andrewayman29@gmail.com>
2026-06-26 01:26:06 +0300

    updates on cta section

--- src/app/components/home-components/cta/cta.ts
+++ src/app/components/home-components/cta/cta.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cta',
   templateUrl: './cta.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
 })
 export class Cta {}
