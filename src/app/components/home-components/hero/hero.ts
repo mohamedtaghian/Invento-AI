9057420ec38feac31b592c96201f51f92c9a5dc9
AndrewAyman <andrewayman29@gmail.com>
2026-06-26 01:25:09 +0300

    updates on hero section

--- src/app/components/home-components/hero/hero.ts
+++ src/app/components/home-components/hero/hero.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
 })
 export class Hero {}
