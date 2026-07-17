f0e6a7b93a4bf4f6e8e51266caee9f88ac417719
Ali Nader <loloafify@gmail.com>
2026-07-17 14:27:26 +0300

    feat(chatbot): implement chatbot design

--- apps/userSite/src/app/app.ts
+++ apps/userSite/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Chatbot } from './components/chatbot/chatbot';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Chatbot],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
