b64d5287c80dd0b0fe2ef0f47fdd11d0b7f9a646
youssefemadsalem <youssefemadeldin22@gmail.com>
2026-07-17 04:18:15 +0300

    feat:inital design  and  logic for  checkout ,orderconfirmed,error404 page

--- apps/userSite/src/app/app.routes.ts
+++ apps/userSite/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout';
+import { NotFoundComponent } from './components/not-found/not-found';
+import { OrderConfirmedComponent } from './components/order-confirmed/order-confirmed';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'order-confirmed', component: OrderConfirmedComponent },
+
+  // The wild component (404) MUST go at the very bottom
+  { path: '**', component: NotFoundComponent },
+];
