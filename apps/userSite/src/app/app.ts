b3fe7d58daa767fa85897d492b8b5203dde0c258
Ali Nader <loloafify@gmail.com>
2026-07-18 18:52:45 +0300

    feat(navbar): implement navbar in user-site

--- apps/userSite/src/app/app.ts
+++ apps/userSite/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Chatbot } from './components/chatbot/chatbot';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
-  imports: [RouterModule, Chatbot],
+  imports: [RouterModule, Chatbot, Navbar],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
