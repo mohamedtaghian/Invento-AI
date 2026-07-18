6e57e52c1d6e62e0cfb59e4955ce49d1486a5e1b
Ali Nader <loloafify@gmail.com>
2026-07-18 21:09:22 +0300

    feat(footer): implement footer in user-site

--- apps/userSite/src/app/app.ts
+++ apps/userSite/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Chatbot } from './components/chatbot/chatbot';
+import { Footer } from './components/footer/footer';
 
 @Component({
-  imports: [RouterModule, Chatbot],
+  imports: [RouterModule, Chatbot, Footer],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ apps/userSite/src/app/components/footer/footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HlmButtonImports } from '@spartan/helm/button';
+
+@Component({
+  selector: 'app-footer',
+  imports: [HlmButtonImports],
+  templateUrl: './footer.html',
+})
+export class Footer {}
