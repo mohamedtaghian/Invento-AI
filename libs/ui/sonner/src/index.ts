477f45d331af3a1dd94b353a70154b55e0bdf229
Mohamed Ashraf Taghian <mohamedtaghian12@gmail.com>
2026-08-14 01:49:05 +0300

    feat(ui): export toast from spartan sonner library

--- libs/ui/sonner/src/index.ts
+++ libs/ui/sonner/src/index.ts
@@ -1,5 +1,7 @@
 import { HlmToaster } from './lib/hlm-toaster';
 
 export * from './lib/hlm-toaster';
+export { toast } from '@spartan-ng/brain/sonner';
 
 export const HlmToasterImports = [HlmToaster] as const;
+
